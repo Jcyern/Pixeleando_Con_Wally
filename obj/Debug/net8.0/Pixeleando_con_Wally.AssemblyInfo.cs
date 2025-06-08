fbc74b0fe4984ab65cbcb050ca128fe3be8849cd
Jcyern <juancarlosyernoespinosa@gmail.com>
2025-06-08 19:02:37 -0400

    GetColorCount

--- obj/Debug/net8.0/Pixeleando_con_Wally.AssemblyInfo.cs
+++ obj/Debug/net8.0/Pixeleando_con_Wally.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pixeleando_con_Wally")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c61c6ab66449a615fbbb1cc48200b6262f24b72a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59cbefbe29ac7292fa2160c51b1ba6b65520c656")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pixeleando_con_Wally")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pixeleando_con_Wally")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ src/Interprete/Parser/Arbol de Sintaxis Abstracta/Funciones/GetColorCount.cs
@@ -0,0 +1,124 @@
+
+using ArbolSintaxisAbstracta;
+using Errores;
+using Evalua;
+using Expresion;
+using ExpressionesTipos;
+
+namespace Arbol_de_Sintaxis_Abstracta
+{
+    public class GetColorCountNode : AstNode
+    {
+        Token get;
+        Expression? Color;
+        Expression? X1;
+        Expression? X2;
+
+        Expression? Y1;
+        Expression? Y2;
+
+        public GetColorCountNode(Token get, Expression? Color, Expression? X1, Expression? Y1, Expression? X2, Expression? Y2)
+        {
+            this.get = get;
+            this.Color = Color;
+            this.X1 = X1;
+            this.Y1 = Y1;
+            this.X2 = X2;
+            this.Y2 = Y2;
+        }
+
+        public override bool CheckSemantic(ExpressionTypes tipo = ExpressionTypes.nothing)
+        {
+            bool chek_x1 = true;
+            bool chek_y1 = true;
+            bool chek_x2 = true;
+            bool chek_y2 = true;
+            bool chek_color = true;
+            if (X1 == null)
+            {
+                //agregar error de compilacion 
+                compilingError.Add(new ExpectedType(get.Pos, ExpressionTypes.Number.ToString(), ExpressionTypes.Null.ToString()));
+                chek_x1 = false;
+            }
+            else
+            {
+                chek_x1 = X1.CheckSemantic(ExpressionTypes.Number);
+            }
+
+            if (Y1 == null)
+            {
+                compilingError.Add(new ExpectedType(get.Pos, ExpressionTypes.Number.ToString(), ExpressionTypes.Null.ToString()));
+                chek_y1 = false;
+            }
+            else
+            {
+                chek_y1 = Y1.CheckSemantic(ExpressionTypes.Number);
+            }
+
+            if (X2 == null)
+            {
+                compilingError.Add(new ExpectedType(get.Pos, ExpressionTypes.Number.ToString(), ExpressionTypes.Null.ToString()));
+                chek_x2 = false;
+            }
+            else
+            {
+                chek_x2 = X2.CheckSemantic(ExpressionTypes.Number);
+            }
+            if (Y2 == null)
+            {
+                compilingError.Add(new ExpectedType(get.Pos, ExpressionTypes.Number.ToString(), ExpressionTypes.Null.ToString()));
+                chek_y2 = false;
+            }
+            else
+            {
+                chek_y2 = Y2.CheckSemantic(ExpressionTypes.Number);
+            }
+            if (Color == null)
+            {
+                compilingError.Add(new ExpectedType(get.Pos, ExpressionTypes.Color.ToString(), ExpressionTypes.Null.ToString()));
+                chek_color = false;
+            }
+            else
+            {
+                chek_color = Color.CheckSemantic(ExpressionTypes.Color);
+            }
+
+
+            if (!chek_x1 || !chek_y1 || !chek_x2 || !chek_y2 || !chek_color)
+            {
+                return false;
+            }
+            else
+                return true;
+
+
+        }
+
+        public override object? Evaluate(Evaluator? evaluador = null)
+        {
+            System.Console.WriteLine("Evaluate de  GetColorCount");
+            System.Console.WriteLine("Aun no se ha definido ");
+            return 0;
+        }
+
+        public override ExpressionTypes GetTipo()
+        {
+            if (Color != null && X1 != null && X2 != null && Y1 != null && Y2 != null)
+            {
+                var c = Color.GetTipo();
+                var x1 = X1.GetTipo();
+                var x2 = X2.GetTipo();
+                var y1 = Y1.GetTipo();
+                var y2 = Y2.GetTipo();
+
+                if (x1 == ExpressionTypes.Number && x2 == ExpressionTypes.Number && y1 == ExpressionTypes.Number && y2 == ExpressionTypes.Number && c == ExpressionTypes.Color)
+                {
+                    return ExpressionTypes.Number;
+                }
+                else
+                    return ExpressionTypes.Invalid;
+            }
+            return ExpressionTypes.Null;
+        }
+    }
+}
